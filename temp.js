29b9178d92afd16ecc23fb75d74f5701d81f1c4f
mars11_pro <luo2863298541@163.com>
2021-04-08 17:29:09 +0800

    mars11_temp.js first document

---
+++ temp.js
@@ -0,0 +1,41 @@
+// function diao() {
+//     abc(function(i) { console.log('n==', i); }, function(i) { console.log('n+1==', i); });
+// }
+
+
+// function abc(success, error) {
+//     var n = 256;
+//     if (2 == 3) {
+//         success(n);
+//     } else {
+//         error(n + 1);
+//     }
+// }
+// diao();
+// // success = function(i) { console.log('n==', i); };
+// // var n = 200;
+// // success(n);
+
+
+// // function  diao()  {
+// //     abc({
+// //         succeed(i)  {  console.log('n==',  i);  },
+// //           error(i)  {  console.log('n+1==',  i);  }
+// //     });
+// // }
+
+// // function  abc(obj)  {
+// //     var  n  =  256;
+// //     if  (2  ==  3)  {    
+// //         obj.succeed(n);
+// //     } 
+// //     else  {    
+// //         obj.error(n  +  1);
+// //     }
+// // }
+// var btn = document.getElementsByTagName('button')[0];
+// console.log(btn);
+console.log(document);
+// setInterval(() => {
+//     console.log('this ===', this);
+// }, 500);
